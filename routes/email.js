9a0ab31a602d04df9936a29c83099aa3e3b18162
Austin Xie <weiyu.xie@gmail.com>
2018-05-09 23:39:14 +1000

    check in mack mail server

---
+++ routes/email.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const cors = require('cors');
+
+const corsOptions = {
+  origin: '*',
+  // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200 
+}
+router.use(cors(corsOptions));
+router.put('/send', function(req, res, next) {
+  console.log('put arrived here: payload', JSON.stringify(req.body));
+  const email = req.body;
+  res.json(email).end();
+});
+
+router.get('/', function(req, res, next) {
+  const email = req.body;
+  res.json("OK").end();
+});
+
+module.exports = router;
